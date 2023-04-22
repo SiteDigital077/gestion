94135f64b2a20f85c16dff6ba55012fcb0c30b22
DesarrolloSite <43462186+DesarrolloSite@users.noreply.github.com>
2023-04-21 23:02:43 -0500

    Ajustes guardado
    Ajustes guardado

--- src/Tenant/Gestion.php
+++ src/Tenant/Gestion.php
@@ -11,7 +11,7 @@ class Gestion extends Model
 	use UsesTenantConnection;
 
 	protected $fillable = [
-    'nombre', 'apellido', 'empresa', 'direccion', 'email', 'numero', 'interes','sector_id','cantidad_id','referido_id','pais_id','ciudad_id','comentarios','estado','nit','tipo',
+    'nombre', 'apellido', 'empresa', 'direccion', 'email', 'numero', 'interes','sector_id','cantidad_id','referido_id','pais_id','ciudad_id','comentarios','estado','nit','tipo','utm_source','utm_medium','utm_campaign',
     ];
 
 	protected $table = 'gestion_usuarios';
